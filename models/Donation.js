300e8e2cbee66ae51f15504b70c54674cf05873d
yana1al <alimasharma@gmail.com>
2024-05-12 22:56:40 -0700

    Updating donation/models

--- models/Donation.js
+++ models/Donation.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const donationSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  location: { type: String, required: true },
-  types: { type: String, required: true },
+  donationType: { type: String, required: true }, 
+  amount: { type: Number }, 
+  date: { type: Date, required: true }, 
+  location: { type: String, required: true }, 
+  donationArea: { type: String }, 
   description: { type: String, required: true },
-  date: { type: String, required: true },
-  
 });
 
 const Donation = mongoose.model("Donation", donationSchema);
