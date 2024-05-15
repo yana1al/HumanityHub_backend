6c7ea2003f644d5d7da8a4d1b8b6fac05507f172
yana1al <alimasharma@gmail.com>
2024-05-14 22:21:42 -0700

    Debugging

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Register, Login } = require("../controllers/authController");
+const { Register, Login } = require("../controllers/auth");
 
 router.post("/register", Register);
 router.post("/login", Login);
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 require("./config/db.connection");
 
+
 const { PORT } = process.env;
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const stripe = require("stripe")("rk_test_51PGOC106R1SUboG0ob27UKItDgwi5JrpL5M5OuBkEoEZoN2x3L5Y1FjKsEKaIg8H1LD5SG9fUOwWHbJJjzJ63823005gRvhwDn")
 
 const authRouter = require("./routes/auth");
 const donationsRouter = require("./routes/donations");
 const eventsRouter = require("./routes/events");
 
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
 app.get("/auth", authRouter);
 app.get("/donations", donationsRouter);
 app.use("/events", eventsRouter);
