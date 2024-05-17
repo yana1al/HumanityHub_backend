29ff52127f2056dab7e365a49d7c1032e17a0aad
yana1al <alimasharma@gmail.com>
2024-05-16 19:50:05 -0700

    Google Auth

--- routes/auth.js
+++ routes/auth.js
@@ -9,4 +9,12 @@ router.post("/login", Login);
 // Google OAuth login route
 router.post("/google-login", googleLogin);
 
+// Google OAuth configuration route
+router.get("/google", (res) => {
+    res.json({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      redirectUri: process.env.GOOGLE_CALLBACK
+    });
+  });
+  
 module.exports = router;
