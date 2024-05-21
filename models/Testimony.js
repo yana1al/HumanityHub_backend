cfc50c8ec2f09dfd98abf209a810572e844dfcc3
yana1al <alimasharma@gmail.com>
2024-05-21 14:15:43 -0700

    fetch events

--- models/Testimony.js
+++ models/Testimony.js
@@ -1,10 +1,11 @@
-
 const mongoose = require('mongoose');
 
-const testimonialSchema = new mongoose.Schema({
+const testimonySchema = new mongoose.Schema({
     name: { type: String, required: true },
     testimony: { type: String, required: true },
     rating: { type: Number, required: true },
+    donatedAmount: { type: Number, required: true },
+    userId: { type: String, required: true } // Assuming userId is a String
 });
 
-module.exports = mongoose.model('Testimonial', testimonialSchema);
+module.exports = mongoose.model('Testimony', testimonySchema);
